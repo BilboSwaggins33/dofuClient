3e37031482ea0a88d1a704ce4f4deb621ac50d20
Aaron Zhang <aarondzhang33@gmail.com>
2023-06-28 14:37:26 -0700

    pre-angular 16

---
+++ src/app/date-title/date-title.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from "@angular/core";
+import { format } from "date-fns";
+
+@Component({
+  selector: 'app-date-title',
+  templateUrl: './date-title.component.html',
+  styleUrls: ['./date-title.component.css']
+})
+export class DateTitleComponent implements OnInit{
+  @Input() date!: Date
+  day!: string
+  dayOfWeek!: string
+  month!: string
+  year!: string
+  constructor() {
+  }
+
+  ngOnInit() {
+    [this.month, this.day, this.dayOfWeek, this.year] = format(this.date, "MMMM/d/eeee/yyyy").split(`/`)
+  }
+}
