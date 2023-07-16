217b0cd6139748fb2eb5d63254d2da41a2b5f80f
Aaron Zhang <aarondzhang33@gmail.com>
2023-07-15 17:00:00 -0700

    initial v1.2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,5 @@
-// export const environment = {
-//   production: false,
-//   apiURL: 'http://localhost:8080'
-// }
-
 export const environment = {
-  production: true,
-  // apiURL: 'http://localhost:8080',
-  apiURL: " https://hidden-hollows-63594-d9a3cf091b67.herokuapp.com/"
+  production: false,
+  // apiURL: 'http://localhost:8080'
+  apiURL: 'https://hidden-hollows-63594-d9a3cf091b67.herokuapp.com'
 }
